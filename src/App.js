492a3db8c39dc46f9b1a034b4180238501d2c55d
Muzammil Haider <muzammilhaider110@gmail.com>
2022-02-10 10:35:43 +0500

    React todo app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [todo, setTodo] = useState([]);
+  let [inputValue, setInputValue] = useState("");
+  const addtodo = () => {
+    todo.push(inputValue);
+    setTodo([...todo]);
+    setInputValue("")
+  };
+  const removeTodo = (index) => {
+    let newTodo = [...todo]
+    newTodo.splice(index,1)
+    setTodo(newTodo)
+  }
+  const deleteTodo = () => {
+    setTodo([]);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="text-center mt-5">TODO LIST</h1>
+      <div className="w-50 mx-auto">
+        <input type="text" placeholder="ENTER TODO..." value={inputValue} className="my-5 form-control input-group" onChange={(e) => setInputValue(e.target.value)} />
+        <button className="btn btn-info mx-2" onClick={addtodo}>ADD TODO</button>
+        <button className="btn btn-danger" onClick={deleteTodo}>DELETE TODO</button>
+      </div>
+      <section className="container my-5">
+        {todo.map((value, index) => {
+          return <li key={index}> {value} 
+            <button onClick={()=>removeTodo(index)} className="btn btn-danger mx-4">Remove</button>
+          </li>;
+        })}
+      </section>
     </div>
   );
 }
